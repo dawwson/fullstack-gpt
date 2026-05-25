d2fff430b56995ae3fc9bdafea246c0b433eeb04
dawwson <shines1427@gmail.com>
2026-05-25 18:27:47 +0900

    add: Parsing Function (#10.3)

--- pages/04_SiteGPT.py
+++ pages/04_SiteGPT.py
@@ -1,6 +1,7 @@
 
+from bs4 import BeautifulSoup
 from langchain_community.document_loaders import SitemapLoader
-from langchain_community.document_transformers import Html2TextTransformer
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 import streamlit as st
 
 
@@ -19,8 +20,6 @@ Start by writing the URL of the website on the sidebar.
 """
 )
 
-html2text_transformer = Html2TextTransformer()
-
 with st.sidebar:
   url = st.text_input(
     "Write down a URL",
@@ -28,10 +27,37 @@ with st.sidebar:
   )
 
 
+# header, footer 제거한 나머지 text 반환
+def parse_page(soup: BeautifulSoup):
+  header = soup.find("header")
+  footer = soup.find("footer")
+  
+  if header:
+    header.decompose()
+  if footer:
+    footer.decompose()
+  
+  return (
+    str(soup.get_text())
+    .replace("\n", " ")
+    .replace("↗", "")
+  )
+
+
+
 @st.cache_data(show_spinner="Loading website...")
 def load_website(url):
+  splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=1000,
+    chunk_overlap=200,
+  )
+
   loader = SitemapLoader(
       url,
+      filter_urls=[
+        r"^(.*\/ai-gateway\/).*", # /ai-gateway/를 포함하는 URL만 필터링
+      ],
+      parsing_function=parse_page,
       # FIXME: rate limit 문제로 추가하였음. 모든 페이지를 불러오지 못하므로 다른 해결방안 모색이 필요함
       blocksize=10, # 10개씩 나눠서 긁어옴
       blocknum=0,
@@ -40,7 +66,9 @@ def load_website(url):
     
   loader.requests_per_second = 1
     
-  return loader.load()
+  docs = loader.load_and_split(text_splitter=splitter)
+
+  return docs
 
 
 if url:
